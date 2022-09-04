69d6d9d32b6fabaca638d1066cc89e5b8c1458f8
Ian Iversen <ianiversen1@gmail.com>
2022-09-04 11:06:27 -0400

    just before flexbox in tutorial

--- css-video-tutorial/src/App.js
+++ css-video-tutorial/src/App.js
@@ -1,9 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <h1>My Website</h1>
+    <>
+      <h1>My Website</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <h2>About Me</h2>
+      <img id="main-image"src="https://pbs.twimg.com/media/E-o6gtkVgAAMzRU.jpg:large" alt="Koalas"></img>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Includar:
+        <ul>
+            <li>Things go here</li>
+            <li>Here is another thing</li>
+            <li>There are three things</li>
+        </ul>
+      </p>
+
+      <h2>Services</h2>
+      <div id="container">
+        <a id="google-link" href="https://www.google.com/">Click here to go to google</a>
+      </div>
+      <table>
+        <tr>
+          <th>Firstname</th>
+          <th>Lastname</th>
+        </tr>
+        <tr>
+          <td>Peter</td>
+          <td>Griffin</td>
+        </tr>
+        <tr>
+          <td>Lois</td>
+          <td>Griffin</td>
+        </tr>
+      </table>
+
+      <div id="gradient"></div>
+
+      <img src="https://i.pinimg.com/originals/e4/a1/74/e4a174cd93aca42b85636cf54b4ad08d.jpg" alt="Single Koala"></img>
+      
+
+    </>
   )
 }
 
